659a75789951c0e47e175e99fd54c94f6540abe9
Tajbir islam <studenttajbirislam@gmail.com>
2025-12-06 00:47:30 +0600

    test

--- handler/message/subscriptionEndMessage.js
+++ handler/message/subscriptionEndMessage.js
@@ -2,7 +2,7 @@ const subscriptionEndMessage = async (email) => {
     const text =`আপনার ${email} এর ChatGPT Plus subscription এর মেয়াদ শেষ হয়েছে।\n
     আপনি কি renew করতে চাচ্ছেন?
     
-    \n*Payment করে থাকলে ignore করুন*
+    \n*Renew অথবা Payment করে থাকলে ignore করুন*
     `
     
     return text
