9d9e3673bd36d8cd8d541f7deed628374358ddbe
Tajbir islam <studenttajbirislam@gmail.com>
2025-12-13 14:43:55 +0600

    test

--- handler/message/subscriptionEndMessage.js
+++ handler/message/subscriptionEndMessage.js
@@ -1,11 +1,13 @@
 const subscriptionEndMessage = async (email) => {
-    const text =`আপনার ${email} এর ChatGPT Plus subscription এর মেয়াদ শেষ হয়েছে।\n
+  const text = `আপনার ${email} এর ChatGPT Plus subscription এর মেয়াদ শেষ হয়েছে।\n
     আপনি কি renew করতে চাচ্ছেন?
     
     _*Renew অথবা Payment করে থাকলে ignore করুন*_
-    `
-    
-    return text
-}
 
-module.exports = subscriptionEndMessage
+    End
+    `;
+
+  return text;
+};
+
+module.exports = subscriptionEndMessage;
--- handler/subscription/subscriptionReminderEndMessage.js
+++ handler/subscription/subscriptionReminderEndMessage.js
@@ -1,12 +1,14 @@
 const subscriptionReminderEndMessage = async (email) => {
-    const text = `আসসালামু আলাইকুম
+  const text = `আসসালামু আলাইকুম
 
 আপনার ${email} এর ChatGPT Plus subscription এর মেয়াদ আর মাত্র ২ দিন বাকি আছে।
 
 আপনি কি renew করতে চাচ্ছেন?
-`
-    
-    return text
-}
 
-module.exports = subscriptionReminderEndMessage
+Reminder
+`;
+
+  return text;
+};
+
+module.exports = subscriptionReminderEndMessage;
