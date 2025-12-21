2982cdfa9a9066be000779f92190423276debdbe
Tajbir islam <studenttajbirislam@gmail.com>
2025-12-22 01:05:45 +0600

    test

--- handler/cronJob.js
+++ handler/cronJob.js
@@ -15,7 +15,7 @@ const setupCronJob = (session, cilents, admin) => {
   // Cron schedule: '0 1 * * *' means every day at 1:00 AM
   // Timezone: Asia/Dhaka (UTC+6)
   cron.schedule(
-    "0 1 * * *",
+    "15 1 * * *",
     async () => {
       const dhakaTime = new Date().toLocaleString("en-US", {
         timeZone: "Asia/Dhaka",
@@ -70,6 +70,7 @@ const setupCronJob = (session, cilents, admin) => {
       }
     },
     {
+      scheduled: true,
       timezone: "Asia/Dhaka",
     }
   );
