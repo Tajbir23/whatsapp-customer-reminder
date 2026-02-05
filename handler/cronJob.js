22dfa12ae1ce83e425bcf97a805b7b51e9741f82
Tajbir23 <studenttajbirislam@gmail.com>
2026-02-05 17:10:50 +0600

    test

--- handler/cronJob.js
+++ handler/cronJob.js
@@ -15,7 +15,7 @@ const setupCronJob = (session, cilents, admin) => {
   // Cron schedule: '0 1 * * *' means every day at 1:00 AM
   // Timezone: Asia/Dhaka (UTC+6)
   cron.schedule(
-    "20 2 * * *",
+    "0 0 * * *",
     async () => {
       const dhakaTime = new Date().toLocaleString("en-US", {
         timeZone: "Asia/Dhaka",
